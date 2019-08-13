be5b156e8d639fc9a57f98bf30071d31a4ba3c04
philyang143 <phil.catanyag@acacia-soft.com>
2019-08-13 16:15:33 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Testing Server
+                    Testing Servers
                 </div>
 
                 <div class="links">
